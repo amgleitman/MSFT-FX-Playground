c8ba13b38569cbbb79252bfbe963b04ff632615d
Chris Hogan <chrishog@microsoft.com>
2020-06-16 10:11:36 -0700

    Stand up initial project

---
+++ Playground/Playground/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Playground
+//
+//  Created by Chris Hogan on 6/16/20.
+//  Copyright © 2020 Microsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
